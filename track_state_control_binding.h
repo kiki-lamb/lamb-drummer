6178590e7db7c6c96424d135a1b627218e3df2cc
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-24 20:41:12 -0400

    legal

--- track_state_control_binding.h
+++ track_state_control_binding.h
@@ -4,28 +4,122 @@
 #include "Arduino.h"
 #include "track_state.h"
 #include "i_buttonpad.h"
+#include "controls.h"
 
+template <class controls_t>
 class TrackStateButtonProcessor {
  public:	
-	static bool handle_button(TrackState & that, IButtonpad::Button button);
-
+   bool handle_button(TrackState & that, typename controls_t::ControlEvent e) {
+      if (e.type < 8) {   
+        (*button_handlers[e.type])(that);
+        return true;
+       }
+      else {
+        ;
+        Serial.print(F("TrackState got unrecognized event: "));
+        Serial.println(e.type);
+      }
+      
+      return false;
+   }
+   
  private:
-  TrackStateButtonProcessor();
-  ~TrackStateButtonProcessor();
+   TrackStateButtonProcessor() {}
+    
+   ~TrackStateButtonProcessor() {}
+        
+   static void increase_mod_maj(TrackState & that) {
+      Serial.println(F("Do BTN_MAJ_UP"));
+      if (that.mod_maj() <= 32)
+        that.set_mod_maj( that.mod_maj() << 1);
+    }
+    
+   static void decrease_mod_maj(TrackState & that) {
+      Serial.println(F("Do BTN_MAJ_DOWN"));
+      if (that.mod_maj() >= 2)
+        that.set_mod_maj( that.mod_maj() >> 1);
+      
+      if (that.phase_maj() >=
+          that.mod_maj())
+        that.set_phase_maj( 0 );
+    
+      if (that.mod_min() >
+          that.mod_maj())
+        that.set_mod_min( that.mod_maj() );
+    
+      if (that.phase_min() >=
+          that.mod_min())
+        that.set_phase_min( 0 );
+    }
+    
+    static void increase_mod_min(TrackState & that) {
+      Serial.println(F("Do BTN_MIN_UP"));
+      if (that.mod_min() < 
+          that.mod_maj() 
+         )
+        that.set_mod_min( that.mod_min() + 1 );
+    }
+    
+    static void decrease_mod_min(TrackState & that) {
+      Serial.println(F("Do BTN_MIN_DOWN"));
+      if (that.mod_min() > 1)
+        that.set_mod_min( that.mod_min()-1 );
+    
+      if (that.phase_min() >=
+          that.mod_min())
+        that.set_phase_min( 0 );
+    }
+    
+    static void increase_phase_maj(TrackState & that) {
+      Serial.println(F("Do BTN_phase_MAJ_UP"));
+      if (that.phase_maj() < that.mod_maj() - 1)
+        that.set_phase_maj( that.phase_maj() + 1);
+      else if (that.phase_maj() ==
+          (that.mod_maj() - 1)
+          ) 
+        that.set_phase_maj( 0 );
+    }
+    
+    static void decrease_phase_maj(TrackState & that) {
+      Serial.println(F("Do BTN_phase_MAJ_DOWN"));
+      if (that.phase_maj() > 0)
+        that.set_phase_maj( that.phase_maj() - 1);
+      else if (that.phase_maj() == 0)
+        that.set_phase_maj( that.mod_maj() - 1);
+    }
+    
+    static void increase_phase_min(TrackState & that) {
+      Serial.println(F("Do BTN_phase_MIN_UP"));
+      if (that.phase_min() < 
+        (that.mod_min() - 1)
+      )
+        that.set_phase_min( that.phase_min() + 1);
+      else if (that.phase_min() == (that.mod_min() - 1)) 
+        that.set_phase_min( 0 );
+    }
+    
+    static void decrease_phase_min(TrackState & that) {
+      Serial.println(F("Do BTN_PHASE_MIN_DOWN"));
+      if (that.phase_min() > 0)
+        that.set_phase_min( that.phase_min() - 1);
+      else if (that.phase_min() == 0)
+        that.set_phase_min( that.mod_min() - 1);
+    }
 
-  static void increase_mod_maj(TrackState & that);
-  static void decrease_mod_maj(TrackState & that);
-  static void increase_mod_min(TrackState & that);
-  static void decrease_mod_min(TrackState & that);
-  static void increase_phase_maj(TrackState & that);
-  static void decrease_phase_maj(TrackState & that);
-  static void increase_phase_min(TrackState & that);
-  static void decrease_phase_min(TrackState & that); 
-
-  typedef void (*button_handler)(TrackState &);
-  static button_handler button_handlers[8];
-
+    typedef void (*button_handler)(TrackState &);
+    static button_handler button_handlers[8];
 };
+
+template <class controls_t> typename TrackStateButtonProcessor<controls_t>::button_handler TrackStateButtonProcessor<controls_t>::button_handlers[8] = {
+  &TrackStateButtonProcessor::increase_mod_min,
+  &TrackStateButtonProcessor::decrease_mod_min,
+  &TrackStateButtonProcessor::increase_mod_maj,
+  &TrackStateButtonProcessor::decrease_mod_maj,
+  &TrackStateButtonProcessor::increase_phase_min,
+  &TrackStateButtonProcessor::decrease_phase_min,
+  &TrackStateButtonProcessor::increase_phase_maj, 
+  &TrackStateButtonProcessor::decrease_phase_maj,    
+};  
 
 #endif
 
