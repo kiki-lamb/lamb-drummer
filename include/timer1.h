187133879c024695b98734b31f53de82a090d328
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-26 09:27:14 -0400

    clean

--- include/timer1.h
+++ include/timer1.h
@@ -5,28 +5,25 @@
 
 class Timer1_ {
   private:
-    static          void    set_hz_by_bpm(uint8_t bpm);
-    static const    uint8_t MIN_BPM;
-    
-    uint8_t   _bpm;
-    bool      _playback_state;    
-    uint8_t   _ticker;
-    double    _hz;
-    
-    static   Timer1_ * _instance;
-    
+    static       void    set_hz_by_bpm(uint8_t bpm);
+    static const uint8_t MIN_BPM;
+    uint8_t              _bpm;
+    bool                 _playback_state;
+    uint8_t              _ticker;
+    double               _hz;
+    static Timer1_ *     _instance;
   public:
-    static Timer1_ & instance();
-    void    setup();
-    void    set_playback_state(bool playback_state_);
-    bool    playback_state() const;
-    void    set_bpm(uint8_t bpm);
-    uint8_t bpm() const;
-    double  hz() const;
-    uint8_t ticker() const;
-    void    increment_ticker();    
-    Timer1_();
+    static Timer1_ &     instance();
+    void                 setup();
+    void                 set_playback_state(bool playback_state_);
+    bool                 playback_state() const;
+    void                 set_bpm(uint8_t bpm);
+    uint8_t              bpm() const;
+    double               hz() const;
+    uint8_t              ticker() const;
+    void                 increment_ticker();
     virtual ~Timer1_();
+    Timer1_();
 };
 
 #endif
