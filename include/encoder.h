f79b4ff05fa0c4d9032f91850aa0325018a9eb6d
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-27 02:00:13 -0400

    ...

--- include/encoder.h
+++ include/encoder.h
@@ -8,7 +8,6 @@ class Encoder {
   private:
     Encoder();
     ~Encoder();
-
     static const    uint8_t                                                     encoderA = 2;
     static const    uint8_t                                                     encoderB = 3;
     static const    uint8_t                                                     ENC_SIG_BITS = 8;
@@ -16,7 +15,6 @@ class Encoder {
     typedef         lamb::Encoders::Interfaces::NativePins<encoderA, encoderB>  enc_interface_t;
     typedef         lamb::Encoders::Custom<enc_state_t, enc_interface_t>        enc_t;
     static          enc_t                                                       encoder;
-
   public:
     static void     setup();
     static void     set_value(uint8_t value);
