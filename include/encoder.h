60b2234fc1f53297bf00b401f39c0aedc1160658
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-27 01:32:11 -0400

    rename

--- include/encoder.h
+++ include/encoder.h
@@ -12,9 +12,9 @@ class Encoder {
     static const    uint8_t                                                       encoderA = 2;
     static const    uint8_t                                                       encoderB = 3;
     static const    uint8_t                                                       ENC_SIG_BITS = 8;
-    typedef         lambOS::Encoders::States::UInt<ENC_SIG_BITS>                  enc_state_t;
-    typedef         lambOS::Encoders::Interfaces::NativePins<encoderA, encoderB>  enc_interface_t;
-    typedef         lambOS::Encoders::Custom<enc_state_t, enc_interface_t>        enc_t;
+    typedef         lamb::Encoders::States::UInt<ENC_SIG_BITS>                  enc_state_t;
+    typedef         lamb::Encoders::Interfaces::NativePins<encoderA, encoderB>  enc_interface_t;
+    typedef         lamb::Encoders::Custom<enc_state_t, enc_interface_t>        enc_t;
     static          enc_t                                                         encoder;
     
   public:
