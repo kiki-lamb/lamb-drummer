e163b08a293b53f9d58a3e555e3be142900da197
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-27 05:12:43 -0400

    ...

--- include/application.h
+++ include/application.h
@@ -22,33 +22,34 @@ public:
 private:
   Application();
   ~Application();
-  typedef Buttonpad_PCF8754<0x3F>  buttonpad_t;
+  typedef Buttonpad_PCF8754<0x3F>
+                           buttonpad_t;
   typedef ControlEventSource<Application::buttonpad_t>
-                                   control_event_source_real_t;
+                           control_event_source_real_t;
   typedef PolledEventSource<ControlEvent>
-                                   control_event_source_t;
-  typedef UiData<tracks_t>         ui_data_t;
-  typedef Ui<ui_data_t>            ui_t;
-  static          control_event_source_t *
-                                   control_event_source;
-  static          ui_t             ui;
-  static          ui_data_t        ui_data;
-  static          Eeprom           eeprom;
-  static          Timer1_          timer1;
-  static          Timer2_          timer2;
-  static          tracks_t         _tracks;
-  static          void             set_playback_state(bool playback_state_);
-  static          bool             process_control_event(control_event_source_t::event_t e);
-  static          void             update_ui_data();
-  static          uint8_t          page();
-  static          uint8_t          bpm();
+                           control_event_source_t;
+  typedef UiData<tracks_t> ui_data_t;
+  typedef Ui<ui_data_t>    ui_t;
+  static control_event_source_t *
+                           control_event_source;
+  static ui_t              ui;
+  static ui_data_t         ui_data;
+  static Eeprom            eeprom;
+  static Timer1_           timer1;
+  static Timer2_           timer2;
+  static tracks_t          _tracks;
+  static void              set_playback_state(bool playback_state_);
+  static bool              process_control_event(control_event_source_t::event_t e);
+  static void              update_ui_data();
+  static uint8_t           page();
+  static uint8_t           bpm();
 public:
-  static          void             setup();
-  static          void             loop();
-  static          void             flag_main_screen(); // called by ISR for timer1.
-  static          tracks_t const & tracks();           // called by ISR for time 1.
-  static          void             process_control_events(); // called by ISR for timer2.
-  static          void             save_state();       // called by ISR for timer2.
+  static void              setup();
+  static void              loop();
+  static void              flag_main_screen(); // called by ISR for timer1.
+  static tracks_t const &  tracks();           // called by ISR for time 1.
+  static void              process_control_events(); // called by ISR for timer2.
+  static void              save_state();       // called by ISR for timer2.
 };
 
 #endif
