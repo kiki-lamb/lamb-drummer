9ea66645561b6bea3d681108d059c3319f12b1ef
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-28 10:50:53 -0400

    ...

--- include/ui_data.h
+++ include/ui_data.h
@@ -6,22 +6,21 @@
 template <class tracks_t>
 class UiData {
 public:
-  tracks_t const *
-                tracks;
-  uint8_t       page;
-  uint8_t       bpm;
-  double        hz;
-  bool          playback_state;
-  uint8_t       ticker;
-  lamb::Flag    popup_bpm_requested;
-  lamb::Flag    redraw_track;
-  lamb::Flag    redraw_selected_track_indicator;
-  lamb::Flag    redraw_playback_state;
+  tracks_t const * tracks;
+  uint8_t          page;
+  uint8_t          bpm;
+  double           hz;
+  bool             playback_state;
+  uint8_t          ticker;
+  lamb::Flag       popup_bpm_requested;
+  lamb::Flag       redraw_track;
+  lamb::Flag       redraw_selected_track_indicator;
+  lamb::Flag       redraw_playback_state;
   UiData() :
-    popup_bpm_requested            ("pbr", true),
-    redraw_track                   ("rt", true),
+    popup_bpm_requested            ("pbr" , true),
+    redraw_track                   ("rt"  , true),
     redraw_selected_track_indicator("rsti", true),
-    redraw_playback_state          ("rps", true)
+    redraw_playback_state          ("rps" , true)
   {}
   virtual ~UiData() {};
 };
