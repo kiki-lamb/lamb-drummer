bcf1be0a8635b7a5dde987f596a037513d38aab3
Kiki Lamb <miss.kiki.lamb@gmail.com>
2019-03-28 22:36:13 -0400

    ...

--- src/application.cpp
+++ src/application.cpp
@@ -44,7 +44,7 @@ void Application::setup() {
   timer1 .set_bpm(tmp.bpm);
   timer2 .setup();
   set_playback_state(tmp.playback_state);
-  while (_tracks.increment_index());
+  while (_tracks++);
   eeprom .unflag_save_requested();
   ui_data.tracks = &_tracks;
   update_ui_data();
