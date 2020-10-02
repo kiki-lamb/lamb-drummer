85b57659dc3b0108ddc6c39df71129c519d8206d
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-01 23:36:18 -0400

    spacing

--- include/ui/ui.h
+++ include/ui/ui.h
@@ -8,40 +8,38 @@
 
 template <class data_t>
 class Ui {
-  friend class SSMain;
+public:
+  enum screen_t {
+    SCREEN_INTRO,
+    SCREEN_NONE,
+    SCREEN_MAIN,
+    SCREEN_INSTR
+  };
+  
+  void update_screen() {
+    screens[current_screen]->update();
+  }
 
-  public:
-    enum screen_t {
-			SCREEN_INTRO,
-			SCREEN_NONE,
-			SCREEN_MAIN,
-			SCREEN_INSTR
-		};
+  void enter_screen(screen_t screen) {
+    if (screen == current_screen)
+      return;
+    current_screen = screen;
+    flag_screen(current_screen);
+    screens[current_screen]->enter();
+  }
 
-    void update_screen() {
-      screens[current_screen]->update();
-    }
+  void setup() {
+    Lcd::setup();
+  }
 
-    void enter_screen(screen_t screen) {
-      if (screen == current_screen)
-        return;
-      current_screen = screen;
-      flag_screen(current_screen);
-      screens[current_screen]->enter();
-    }
+  void flag_screen(screen_t screen) {
+    screens[screen]->flag();
+  }
 
-    void setup() {
-      Lcd::setup();
-    }
-
-    void flag_screen(screen_t screen) {
-      screens[screen]->flag();
-    }
-
-    Ui(data_t * data_) :
-      data(data_),
-			current_screen(SCREEN_NONE),
-      screens()
+  Ui(data_t * data_) :
+    data(data_),
+    current_screen(SCREEN_NONE),
+    screens()
     {
       screens[0] = new SSIntro(data);
       screens[1] = new SSNone (data);
@@ -49,12 +47,12 @@ class Ui {
       screens[3] = new SSInstr(data);
     }
 
-    virtual ~Ui() {}
+  virtual ~Ui() {}
 
-  private:
-    data_t * data;
-    screen_t               current_screen;
-    Screen<data_t> * screens[4];
+private:
+  data_t         * data;
+  screen_t         current_screen;
+  Screen<data_t> * screens[4];
 };
 
 #endif
