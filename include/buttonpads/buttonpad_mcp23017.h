32e21a57758707e8ef9454994e2aeca029b4dfcf
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-02 01:28:17 -0400

    Locking kind of works?

--- include/buttonpads/buttonpad_mcp23017.h
+++ include/buttonpads/buttonpad_mcp23017.h
@@ -5,63 +5,84 @@
 #include "buttonpad.h"
 #include <Wire.h>
 #include "Adafruit_MCP23017.h"
+#include "i2c_lock.h"
 
 template <uint8_t i2c_addr_>
 class Buttonpad_MCP23017 : public Buttonpad {
-  private:
-    uint8_t           _button;
-    Adafruit_MCP23017 device;
+private:
+  uint8_t           _button;
+  Adafruit_MCP23017 device;
 
-  public:
-    Buttonpad_MCP23017() : _button(8) {}
+public:
+  Buttonpad_MCP23017() : _button(8) {}
 
-    virtual ~Buttonpad_MCP23017() {}
+  virtual ~Buttonpad_MCP23017() {}
 
-    virtual void impl_setup() {
-      Serial.println(F("Setup Buttonpad_MCP23017..."));
+  virtual void impl_setup() {
+    Serial.println(F("Setup Buttonpad_MCP23017..."));
 
-      Wire.begin();
-      device.begin(i2c_addr_);
+    Wire.begin();
 
-      for (uint8_t ix = 0; ix < 8; ix++) {
-        device.pinMode(ix, INPUT);
-        device.pullUp(ix, HIGH);
-      }
+    device.begin(i2c_addr_);
 
-      Serial.print(F("Done setup Buttonpad_MCP23017."));
+    for (uint8_t ix = 0; ix < 8; ix++) {
+      device.pinMode(ix, INPUT);
+      device.pullUp(ix, HIGH);
     }
 
-    virtual bool impl_read() {
-      sei();
-//      uint8_t tmpval = device.readGPIO(1);
-      uint8_t tmpval = 0xff;
-      cli();
+    Serial.print(F("Done setup Buttonpad_MCP23017."));
+  }
 
-      uint8_t pin = 0;
+  virtual bool impl_read() {
+    if (! I2CLock::claim()) return false;
 
-      for (uint8_t mask = 0b1; pin < 8; pin++, mask <<= 1) {
-          if (! (tmpval & mask)) {
-            break;
-          }
+    char cSREG = SREG;    
+
+    sei();
+
+    uint8_t tmpval = device.readGPIO(1);
+    SREG = cSREG;
+
+    I2CLock::release();
+    
+    Serial.print("READ: ");
+
+    {
+      for(byte mask = 0x80; mask; mask >>= 1) {
+        if(mask  & tmpval)
+          Serial.print('1');
+        else
+          Serial.print('0');
       }
-
-      if (pin != _button) {
-        _button = pin;
-
-        if (_button != 8) {
-          //Serial.print(F("Pressed button "));
-          //Serial.print(pin);
-          //Serial.println();
-          return true;
-        }
-      }
-
-      return false;
     }
 
-    virtual uint8_t impl_button() const {
-      return _button;
+    Serial.println();
+          
+    uint8_t pin = 0;
+
+    for (uint8_t mask = 0b1; pin < 8; pin++, mask <<= 1) {
+      if (! (tmpval & mask)) {
+        break;
+      }
     }
+
+    if (pin != _button) {
+      _button = pin;
+
+      if (_button != 8) {
+        //Serial.print(F("Pressed button "));
+        //Serial.print(pin);
+        //Serial.println();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  virtual uint8_t impl_button() const {
+    return _button;
+  }
 };
 
 #endif
