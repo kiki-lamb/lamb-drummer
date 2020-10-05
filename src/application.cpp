9886ef91f05a055d8e92495deac76738463dde2c
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-05 01:33:32 -0400

    correction, formatting

--- src/application.cpp
+++ src/application.cpp
@@ -36,27 +36,26 @@ lamb::Flag lightsFlag;
 
 void Application::setup() {
   Serial .begin(230400);
+
   Wire   .begin();
   Wire   .setClock(400000);
 
   lightsMcp.begin(0x4);
+
   for (size_t ix = 0; ix < 16; ix++) {
      lightsMcp.pinMode(ix, OUTPUT);
      lightsMcp.digitalWrite(ix, LOW);
   }
 
-  // for (size_t ix = 0; ix < 16; ix++) {
-  //    lightsMcp.digitalWrite(ix, HIGH);
-  //    delay(500);
-  // }
-
   output_device.begin(0x3a);
+
   for (uint8_t ix = 0; ix < 8; ix++) {
     output_device.pinMode(ix, OUTPUT);
   }
 
   output_device.write(0xff);
-  uint16_t step = 150;
+  
+  static const uint16_t step = 150;
 
   ui     .setup();
   ui     .enter_screen(ui_t::SCREEN_INTRO);
@@ -94,11 +93,12 @@ void Application::setup() {
   setup_controls(tmp.bpm);
   
   cli();
+
   Serial.println(F("Stop all interrupts...")); Serial.flush();
   
   timer1 .setup();
-
   timer1 .set_bpm(120);
+
   timer2 .setup();
   
   set_playback_state(tmp.playback_state);
@@ -107,23 +107,26 @@ void Application::setup() {
 
   eeprom .unflag_save_requested();
   eeprom .flag_save_requested();
+
   save_state();
   
   ui_data.tracks = &_tracks;
+
   update_ui_data();
 
   //Serial.println(F("Enable all interrupts.")); Serial.flush();
   sei();
   
   Serial.println(F("Enter SCREEN_MAIN...")); Serial.flush();
+
   ui     .enter_screen(ui_t::SCREEN_MAIN);
+
   Serial.println(F("Entered SCREEN_MAIN.")); Serial.flush();
 }
 
 void Application::setup_controls(uint8_t bpm) {
   static ButtonpadSource<Buttonpad_MCP23017<0x0, 8,  8> > buttonpad_source0;
   static ButtonpadSource<Buttonpad_MCP23017<0x3, 16, 0> > buttonpad_source1;
-
 //  static EncoderSource encoder_source(EventType::EVT_BPM_SET, bpm);
 //  static ButtonSource  button_source(
 //    EventType::EVT_PLAYBACK_STATE_TOGGLE,
@@ -133,12 +136,16 @@ void Application::setup_controls(uint8_t bpm) {
                        combine_event_sources;
   buttonpad_source0     .setup();
   buttonpad_source1     .setup();
+
 //  encoder_source       .setup();
 //  button_source.setup();
+
   combine_event_sources.sources[0] = &buttonpad_source0;
   combine_event_sources.sources[1] = &buttonpad_source1;
+
 //  combine_event_sources.sources[1] = &encoder_source;
 //  combine_event_sources.sources[2] = &button_source;
+
   control_event_source .source     = &combine_event_sources; //combine_event_sources;
 }
 
@@ -164,6 +171,7 @@ bool Application::output() {
     Serial.print(Application::timer1.ticker()); Serial.flush();
     Serial.print(F(" ")); Serial.flush();
     Serial.print(F("Output: ")); Serial.flush();
+
     print_bits(queued_output);
     
     Serial.println(); Serial.flush();
@@ -190,17 +198,7 @@ void Application::loop() {
 
   if (lightsFlag.consume()) {
     Serial.print("Write lights: ");
-
-    uint8_t a = lights >> 8;
-    uint8_t b = lights &  0xff;
-
-    print_bits(a);
-    print_bits(b);
-
-    Serial.println();
-
-    uint16_t c = (((uint16_t)b) << 8) | a;
-  
+    
     lightsMcp.writeGPIOAB(lights);
   }
 }
@@ -212,13 +210,17 @@ void Application::flag_main_screen() {
 uint8_t Application::page() {
   uint8_t tmp_tick        = timer1.ticker() >> 1;
   uint8_t tmp_inside_tick = tmp_tick % _tracks.max_mod_maj();
+
   return                    tmp_inside_tick /  16;
 }
 
 void Application::set_playback_state(bool playback_state_) {
   timer1.set_playback_state(playback_state_);
+
   ui_data.redraw_playback_state.flag();
+
   flag_main_screen();
+
   eeprom.flag_save_requested();
 }
 
@@ -239,7 +241,9 @@ void Application::flag_controls() {
 void Application::flag_output(uint8_t output) {
 #ifdef LOG_OUTPUT
   Serial.print(F("Flagging: ")); Serial.flush();
+  
   print_bits(output);
+
   Serial.println(); Serial.flush();
 #endif
   
@@ -262,19 +266,31 @@ bool Application::process_control_events() {
   return true;
 }
 
+
+uint16_t flip_bytes(uint16_t value) {
+  uint8_t a = value >> 8;
+  uint8_t b = value &  0xff;
+  
+  return (((uint16_t)b) << 8) | a; 
+}
+
 bool Application::process_control_event(
   Application::control_event_source_t::event_t e
 ) {
-  if (! e)
+  if (! e) {
     return false;
+  }
   if ((e.type >= 20) && (e.type <= 27)) {
     ProcessTrackControl<Event::event_type_t, 8>::apply(
       _tracks.current(),
       ((EventType)(e.type - 20))
     );
+    
     ui_data.redraw_track.flag();
     ui_data.redraw_selected_track_indicator.flag();
+
     eeprom.flag_save_requested();
+    
     goto success;
   }
   else {
@@ -300,41 +316,59 @@ bool Application::process_control_event(
       Serial.print(e.type);
       Serial.println();
       Serial.flush();      
-      uint16_t tmp = ((uint16_t)1) << e.type;
-      uint8_t  a = tmp >> 8;
-      uint8_t  b = tmp &  0xff;      
-      uint16_t c = (((uint16_t)b) << 8) | a;
+
+      uint16_t tmp = flip_bytes(((uint16_t)1) << e.type);
       
-      lights ^= c;
+      lights ^= tmp;
       
       lightsFlag.flag();
+
       goto success;
     }
     case EventType::EVT_SELECTED_TRACK_UP:
+    {
       _tracks++;
+
       ui_data.redraw_track.flag();
       ui_data.redraw_selected_track_indicator.flag();
+
       Serial.print("GO UP A TRACK TO "); Serial.flush();
       Serial.print(_tracks.index()); //  Serial.flush();
       Serial.println(); Serial.flush();
+
       break;
+    }
     case EventType::EVT_SELECTED_TRACK_DN:
+    {
       _tracks--;
+      
       ui_data.redraw_track.flag();
       ui_data.redraw_selected_track_indicator.flag();
+      
       Serial.print("GO DOWN A TRACK TO "); Serial.flush();
       Serial.print(_tracks.index()); Serial.flush();
       Serial.println(); Serial.flush();
-      break;      
+      
+      break;
+    }
     case EventType::EVT_PLAYBACK_STATE_TOGGLE:
+    {
       set_playback_state(! timer1.playback_state());
+      
       eeprom.flag_save_requested();
+      
       goto success;
+    }
     case EventType::EVT_BPM_SET:
+    {
       timer1.set_bpm(e.parameter);
+
       ui_data.popup_bpm_requested.flag();
+      
       eeprom.flag_save_requested();
+      
       goto success;
+    }
     }
     return false;
 
