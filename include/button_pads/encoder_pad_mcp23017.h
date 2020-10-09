c37ece6b9be27886031ddc132b6479786ed81253
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-09 04:45:27 -0400

    encoder_state gets it's own file

--- include/button_pads/encoder_pad_mcp23017.h
+++ include/button_pads/encoder_pad_mcp23017.h
@@ -4,44 +4,10 @@
 #include <inttypes.h>
 #include "lamb.h"
 #include "button_pads/pad_mcp23017.h"
+#include "encoder_state/encoder_state.h"
 
 template <uint8_t encoder_count>
 class encoder_pad_mcp23017 : public pad_mcp23017 {
-private:
-  class encoder_state {
-  public:
-    int8_t  motion;
-    uint8_t stage_ix;
-    bool    flagged;
-     
-    encoder_state() : motion(0), stage_ix(0), flagged(false) {}
-    
-    ~encoder_state() {}
-     
-    void update(char bit_pair) {
-      static const unsigned char stages[] = { 0b00, 0b01, 0b11, 0b10 };
-       
-      if (stages[((uint8_t)(stage_ix + 1U)) & 0b11] == bit_pair) {
-        this->motion--;
-
-        if (motion < -1) {
-          this->flagged = true;
-        }
-
-        this->stage_ix = (stage_ix + 1U) & 0b11;
-      }
-      else if (stages[((uint8_t)(stage_ix - 1)) & 0b11] == bit_pair) {
-        this->motion++;
-
-        if (motion > 1) {
-          this->flagged = true;
-        }
-
-        this->stage_ix = (this->stage_ix - 1) & 0b11;
-      }
-    }
-  };
-  
 private:
   encoder_state encoder_states[encoder_count];
   
---
+++ include/encoder_state/encoder_state.h
@@ -0,0 +1,40 @@
+#ifndef LAMB_DRUMMER_ENCODER_STATE
+#define LAMB_DRUMMER_ENCODER_STATE
+
+#include <inttypes.h>
+
+class encoder_state {
+public:
+  int8_t  motion;
+  uint8_t stage_ix;
+  bool    flagged;
+  
+  encoder_state() : motion(0), stage_ix(0), flagged(false) {}
+  
+  virtual ~encoder_state() {}
+     
+  virtual void update(char bit_pair) {
+    static const unsigned char stages[] = { 0b00, 0b01, 0b11, 0b10 };
+       
+    if (stages[((uint8_t)(stage_ix + 1U)) & 0b11] == bit_pair) {
+      this->motion--;
+
+      if (motion < -1) {
+        this->flagged = true;
+      }
+
+      this->stage_ix = (stage_ix + 1U) & 0b11;
+    }
+    else if (stages[((uint8_t)(stage_ix - 1)) & 0b11] == bit_pair) {
+      this->motion++;
+
+      if (motion > 1) {
+        this->flagged = true;
+      }
+
+      this->stage_ix = (this->stage_ix - 1) & 0b11;
+    }
+  }
+};
+
+#endif
