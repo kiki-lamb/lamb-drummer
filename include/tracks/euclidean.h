1a8221b33a76d4abd7dabdac7e9f068cf82e7a55
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-09 20:16:09 -0400

    change define

--- include/tracks/euclidean.h
+++ include/tracks/euclidean.h
@@ -1,5 +1,5 @@
-#ifndef LAMB_DRUMMER_TRACK_STATE_H
-#define LAMB_DRUMMER_TRACK_STATE_H
+#ifndef LAMB_DRUMMER_EUCLIDEAN_TRACK_STATE_H
+#define LAMB_DRUMMER_EUCLIDEAN_TRACK_STATE_H
 
 #include "Arduino.h"
 #include <lamb.h>
