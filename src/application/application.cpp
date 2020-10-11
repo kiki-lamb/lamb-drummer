f32dd8fe74a1561a74f1a1c95254dae6a5be03f6
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-10 22:17:44 -0400

    clean

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -21,56 +21,61 @@ Adafruit_MCP23017             application::_encoder_pad_device;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-application::control_source  application::_control_event_source;
+lamb::controls::encoder_pad_mcp23017
+application::_combo_pad_encoder_pad(
+  application::combo_pad_encoders_count,
+  application::combo_pad_encoders_range_start
+);
 
-application::combined_source application::_combine_event_sources;
+lamb::controls::button_pad_mcp23017
+application::_combo_pad_button_pad(
+  application::combo_pad_buttons_count,
+  application::combo_pad_buttons_range_start
+);
+
+lamb::controls::encoder_pad_mcp23017
+application::_encoder_pad0(
+  application::encoder_pad_encoders_count,
+  application::encoder_pad_encoders_range_start
+);
+
+lamb::controls::button_pad_mcp23017
+application::_drum_pad_button_pad(
+  application::drum_pad_buttons_count,
+  application::drum_pad_buttons_range_start
+);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-lamb::controls::encoder_pad_mcp23017   application::_combo_pad_encoder_pad(
-                         application::combo_pad_encoders_count,
-                         application::combo_pad_encoders_range_start
-                       );
-
-lamb::controls::button_pad_mcp23017    application::_combo_pad_button_pad(
-                         application::combo_pad_buttons_count,
-                         application::combo_pad_buttons_range_start
-                       );
-////////////////////////////////////////////////////////////////////////////////
-
-lamb::controls::encoder_pad_mcp23017   application::_encoder_pad0(
-                         application::encoder_pad_encoders_count,
-                         application::encoder_pad_encoders_range_start
-                       );
-
-lamb::controls::button_pad_mcp23017    application::_drum_pad_button_pad(
-                         application::drum_pad_buttons_count,
-                         application::drum_pad_buttons_range_start
-                       );
+application::encoder_pad_source
+application::_encoder_pad_source0(
+  &application::_encoder_pad0,
+  application::encoder_pad_source_mask
+);
 
 application::encoder_pad_source
-                       application::_encoder_pad_source0(
-                         &application::_encoder_pad0,
-                         application::encoder_pad_source_mask
-                       );
-
-application::encoder_pad_source
-                       application::_combo_pad_encoder_source(
-                         &application::_combo_pad_encoder_pad,
-                         application::combo_pad_encoders_source_mask
-                       );
+application::_combo_pad_encoder_source(
+  &application::_combo_pad_encoder_pad,
+  application::combo_pad_encoders_source_mask
+);
 
 application::button_pad_source
-                       application::_combo_pad_button_source(
-                         &application::_combo_pad_button_pad,
-                         application::combo_pad_buttons_source_mask
-                       );
+application::_combo_pad_button_source(
+  &application::_combo_pad_button_pad,
+  application::combo_pad_buttons_source_mask
+);
 
 application::button_pad_source
-                       application::_drum_pad_source(
-                         &application::_drum_pad_button_pad,
-                         application::drum_pad_buttons_source_mask
-                       );
+application::_drum_pad_source(
+  &application::_drum_pad_button_pad,
+  application::drum_pad_buttons_source_mask
+);
+
+application::control_source
+application::_control_event_source;
+
+application::combined_source
+application::_combine_event_sources;
 
 ////////////////////////////////////////////////////////////////////////////////
 
