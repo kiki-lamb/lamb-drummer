a456785532f51e605c9a7a48c6ad7f50f6bc6a40
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-12 10:47:32 -0400

    font

--- src/screens/lcd.cpp
+++ src/screens/lcd.cpp
@@ -161,10 +161,10 @@ void lcd::select_inversion(uint8_t const & number) {
       0b11101
     },
     {
+      0b11111,
       0b00000,
       0b01111,
       0b00001,
-      0b11110,
       0b11110,
       0b01110,
       0b10001,
@@ -177,8 +177,8 @@ void lcd::select_inversion(uint8_t const & number) {
       0b00001,
       0b01110,
       0b01110,
-      0b01110,
-      0b10001
+      0b10001,
+      0b11111
     }
   };
 
